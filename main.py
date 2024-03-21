91bd5a7c2e4804743c230ea45469fb759e9f2ce7
RockChinQ <1010553892@qq.com>
2024-03-21 11:02:30 +0800

    feat: 更新以适配 3.1 版本的 API

--- main.py
+++ main.py
@@ -1,45 +1,48 @@
-from pkg.plugin.models import *
-from pkg.plugin.host import EventContext, PluginHost
+from pkg.plugin.context import register, handler, llm_func, BasePlugin, APIHost, EventContext
+from pkg.plugin.events import *  # 导入事件类
 
 
 # 注册插件
 @register(name="Hello", description="hello world", version="0.1", author="RockChinQ")
-class MyPlugin(Plugin):
+class MyPlugin(BasePlugin):
 
     # 插件加载时触发
-    # plugin_host (pkg.plugin.host.PluginHost) 提供了与主程序交互的一些方法，详细请查看其源码
-    def __init__(self, plugin_host: PluginHost):
+    def __init__(self, host: APIHost):
+        pass
+
+    # 异步初始化
+    async def initialize(self):
         pass
 
     # 当收到个人消息时触发
-    @on(PersonNormalMessageReceived)
-    def person_normal_message_received(self, event: EventContext, **kwargs):
-        msg = kwargs['text_message']
+    @handler(PersonNormalMessageReceived)
+    async def person_normal_message_received(self, ctx: EventContext):
+        msg = ctx.event.text_message  # 这里的 event 即为 PersonNormalMessageReceived 的对象
         if msg == "hello":  # 如果消息为hello
 
             # 输出调试信息
-            logging.debug("hello, {}".format(kwargs['sender_id']))
+            self.ap.logger.debug("hello, {}".format(ctx.event.sender_id))
 
             # 回复消息 "hello, <发送者id>!"
-            event.add_return("reply", ["hello, {}!".format(kwargs['sender_id'])])
+            ctx.add_return("reply", ["hello, {}!".format(ctx.event.sender_id)])
 
             # 阻止该事件默认行为（向接口获取回复）
-            event.prevent_default()
+            ctx.prevent_default()
 
     # 当收到群消息时触发
-    @on(GroupNormalMessageReceived)
-    def group_normal_message_received(self, event: EventContext, **kwargs):
-        msg = kwargs['text_message']
+    @handler(GroupNormalMessageReceived)
+    async def group_normal_message_received(self, ctx: EventContext):
+        msg = ctx.event.text_message  # 这里的 event 即为 GroupNormalMessageReceived 的对象
         if msg == "hello":  # 如果消息为hello
 
             # 输出调试信息
-            logging.debug("hello, {}".format(kwargs['sender_id']))
+            self.ap.logger.debug("hello, {}".format(ctx.event.sender_id))
 
             # 回复消息 "hello, everyone!"
-            event.add_return("reply", ["hello, everyone!"])
+            ctx.add_return("reply", ["hello, everyone!"])
 
             # 阻止该事件默认行为（向接口获取回复）
-            event.prevent_default()
+            ctx.prevent_default()
 
     # 插件卸载时触发
     def __del__(self):
